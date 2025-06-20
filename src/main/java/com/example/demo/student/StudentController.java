873fe2a603fe559a5748a1b0112e3e06c7ebc78c
Nandan <nandansanjay35@gmail.com>
2025-06-20 12:24:52 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    private final StudentService studentservice;
+
+
+    public StudentController(StudentService studentservice) {
+        this.studentservice = studentservice;
+    }
+
+    @GetMapping
+    public List<Student> getstudents(){
+        return studentservice.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentservice.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId")Long Studentid){
+        studentservice.deleteStudent(Studentid);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false)String name,
+            @RequestParam(required = false)String email){
+        studentservice.updateStudent(studentId,name,email);
+    }
+
+}
